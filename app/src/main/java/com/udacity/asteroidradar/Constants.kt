c36085569e397914740af11c40925ab469bc0d77
Alexander Schlenk <alexander@schlenk.at>
2024-01-14 20:18:00 +0100

    version 3

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "LggXT1jYiRuteyoPSLYpkLWfx5Rpr3cvjWjh3LDP"
+    const val API_KEY = "" // TODO place APi key here
 }
